69abd4c09ca151279d8acfae33271351ba901125
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-30 15:56:30 +0500

    Program duration and menus communication provided

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DormitoryManagementSystem.Menus;
+
+namespace DormitoryManagementSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        RoomMenu roomMenu = new RoomMenu();
+
+        string prorgamFlow = string.Empty;
+
+        do
+        {
+            roomMenu.BaseMenu();
+            
+            Console.Write("Select the neccesary section: ");
+            int option = int.Parse(Console.ReadLine());
+            roomMenu.MenuSelection(option);
+
+            Console.WriteLine("Do you want to exit the program?");
+            Console.Write("(yes/no): ");
+            prorgamFlow = Console.ReadLine();
+        } while (prorgamFlow.Equals("no"));
+    }
+}
